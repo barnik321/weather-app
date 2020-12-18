93ec176416705f6114bf988050935134a39f2235
Barnik Biswas <42123164+dopApod17@users.noreply.github.com>
2020-12-18 20:29:28 +0530

    Change the name of app object

--- wqu_app/app.py
+++ wqu_app/app.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, request
 from message import greet
 
-app = Flask(__name__)
+my_app = Flask(__name__)
 
 
-@app.route('/')
+@my_app.route('/')
 def main():
     ip = request.headers['X-Forwarded-For']
     return render_template('index.html', message=greet(ip))
 
 
 if __name__ == '__main__':
-    app.env = 'development'
-    app.run(debug=True)
+    my_app.env = 'development'
+    my_app.run(debug=True)
